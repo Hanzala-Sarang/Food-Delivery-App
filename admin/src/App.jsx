abc1a3c294a197f2c0c79c7c1e8e50bbbd807406
Hanzala-Sarang <126908905+Hanzala-Sarang@users.noreply.github.com>
2024-07-03 14:49:45 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4500";
+  const url = "https://food-delivery-app-i3ep.onrender.com";
   return (
     <div>
       <ToastContainer />
